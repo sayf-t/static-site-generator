fcfe2928ac8e9ab04f066a73144d35388abe9338
sayf-t <sayfcodes@gmail.com>
2025-05-10 08:53:24 +0800

    Add new Python files for text node functionality, remove unused HTML and CSS files, and update .gitignore to exclude SpecStory auto-save files.

---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image" 
+
+class TextNode:
+    def __init__(self, text, type, url):
+        self.text = text
+        self.type = type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.type == other.type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.type.value}, {self.url})"
+    
